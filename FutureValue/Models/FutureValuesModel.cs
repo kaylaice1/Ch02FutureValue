6f047359b97cdc45201475d8ecbf708f6898b7bb
KayLynn Johnson <johnson.kaylynn@outlook.com>
2024-09-08 11:06:44 -0500

    Step 8-12

---
+++ FutureValue/Models/FutureValuesModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FutureValue.Models
+{
+    public class FutureValuesModel
+    {
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+        public decimal CalculateFutureValue()
+        {
+            int months = Years * 12;
+            decimal MonthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal futureValue = 0;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) *
+                                (1 + MonthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
